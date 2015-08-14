438da910523ffb84f33a46285fb0cfe0091daf09
ramsauerd89cs <ramsauerd89cs@192.168.0.133>
2015-08-14 17:13:40 +0200

    classifier methods implemented

--- WekaMachineLearningWorkbench/src/StartWeka.java
+++ WekaMachineLearningWorkbench/src/StartWeka.java
@@ -101,232 +101,35 @@ public class StartWeka {
 		
 		Evaluation eval=null;
 		
-		
+		try {
+			eval = new Evaluation(train);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		
-//*******************************BAYES NET Default values all 160 attributes*******************************//
-//			BayesNet bN = new BayesNet();
-//			try {
-//				bN.buildClassifier(train);
-//			} catch (Exception e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-			try {
-				eval = new Evaluation(train);
-////				eval.crossValidateModel(bN,train,10,new Random(1));
-//				eval.evaluateModel(bN,train);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			Attribute classNames = train.classAttribute();
-			
-////			header = "class name"+sepCSV+"truePositiveRate"+sepCSV+"falsePositiveRate"+sepCSV+"Precision"+sepCSV+"Recall"+sepCSV+"fMeasure"+sepCSV+"ROC"+ls;
-//			
-////			classNames = train.classAttribute();
-//			class1 = classNames.value(0)+sepCSV+eval.truePositiveRate(0)+sepCSV+eval.falsePositiveRate(0)+sepCSV+eval.precision(0)+sepCSV+eval.recall(0)+sepCSV+eval.fMeasure(0)+sepCSV+eval.areaUnderROC(0);
-//			class2 = classNames.value(1)+sepCSV+eval.truePositiveRate(1)+sepCSV+eval.falsePositiveRate(1)+sepCSV+eval.precision(1)+sepCSV+eval.recall(1)+sepCSV+eval.fMeasure(1)+sepCSV+eval.areaUnderROC(1);
-//			class3 = classNames.value(2)+sepCSV+eval.truePositiveRate(2)+sepCSV+eval.falsePositiveRate(2)+sepCSV+eval.precision(2)+sepCSV+eval.recall(2)+sepCSV+eval.fMeasure(2)+sepCSV+eval.areaUnderROC(2);
-//			class4 = classNames.value(3)+sepCSV+eval.truePositiveRate(3)+sepCSV+eval.falsePositiveRate(3)+sepCSV+eval.precision(3)+sepCSV+eval.recall(3)+sepCSV+eval.fMeasure(3)+sepCSV+eval.areaUnderROC(3);
-//			classifierName="BayesNet Default values all 160 attributes"+testMethod;
-//			evalString += classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
-//		
+		Attribute classNames = train.classAttribute();
+				
 		int numFolds = 0;
 		int randomSeed = 0;
 		boolean isFold = true;
 		boolean isTrainingTest = true;
+
+		
+//*******************************BAYES NET Default values all 160 attributes*******************************//
+		
 		evalString += bayesNetFct(eval, train, classNames, "TrainingIsTestData", !isFold, numFolds, randomSeed, isTrainingTest);
 
 //*******************************NAIVE BAYES Default values all 160 attributes*******************************//
 		
-		NaiveBayes nB = new NaiveBayes();
-		try {
-			nB.buildClassifier(train);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			eval = new Evaluation(train);
-//			eval.crossValidateModel(nB,train,10,new Random(1));
-			eval.evaluateModel(nB,train);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			
-//		Attribute classNames = train.classAttribute();
-		class1 = classNames.value(0)+sepCSV+eval.truePositiveRate(0)+sepCSV+eval.falsePositiveRate(0)+sepCSV+eval.precision(0)+sepCSV+eval.recall(0)+sepCSV+eval.fMeasure(0)+sepCSV+eval.areaUnderROC(0);
-		class2 = classNames.value(1)+sepCSV+eval.truePositiveRate(1)+sepCSV+eval.falsePositiveRate(1)+sepCSV+eval.precision(1)+sepCSV+eval.recall(1)+sepCSV+eval.fMeasure(1)+sepCSV+eval.areaUnderROC(1);
-		class3 = classNames.value(2)+sepCSV+eval.truePositiveRate(2)+sepCSV+eval.falsePositiveRate(2)+sepCSV+eval.precision(2)+sepCSV+eval.recall(2)+sepCSV+eval.fMeasure(2)+sepCSV+eval.areaUnderROC(2);
-		class4 = classNames.value(3)+sepCSV+eval.truePositiveRate(3)+sepCSV+eval.falsePositiveRate(3)+sepCSV+eval.precision(3)+sepCSV+eval.recall(3)+sepCSV+eval.fMeasure(3)+sepCSV+eval.areaUnderROC(3);
-		
-		classifierName="NaiveBayes Default Values all 160 attributes"+testMethod;
-		evalString += ls+classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
-		
+		evalString += naiveBayesFct(eval, train, classNames, "TrainingIsTestData", !isFold, numFolds, randomSeed, isTrainingTest);
 		
 //*******************************J48 Default values all 160 attributes*******************************//
-		J48 j48 = new J48();
-		String[] j48Options = new String[4];
-		j48Options[0]="-C";
-		j48Options[1]="0.25";
-		j48Options[2]="-M";
-		j48Options[3]="2";
-		try {
-			j48.setOptions(j48Options);
-		} catch (Exception e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-
-		try {
-			j48.buildClassifier(train);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			eval = new Evaluation(train);
-//			eval.crossValidateModel(j48,train,10,new Random(1));
-			eval.evaluateModel(j48,train);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		classNames = train.classAttribute();
-		class1 = classNames.value(0)+sepCSV+eval.truePositiveRate(0)+sepCSV+eval.falsePositiveRate(0)+sepCSV+eval.precision(0)+sepCSV+eval.recall(0)+sepCSV+eval.fMeasure(0)+sepCSV+eval.areaUnderROC(0);
-		class2 = classNames.value(1)+sepCSV+eval.truePositiveRate(1)+sepCSV+eval.falsePositiveRate(1)+sepCSV+eval.precision(1)+sepCSV+eval.recall(1)+sepCSV+eval.fMeasure(1)+sepCSV+eval.areaUnderROC(1);
-		class3 = classNames.value(2)+sepCSV+eval.truePositiveRate(2)+sepCSV+eval.falsePositiveRate(2)+sepCSV+eval.precision(2)+sepCSV+eval.recall(2)+sepCSV+eval.fMeasure(2)+sepCSV+eval.areaUnderROC(2);
-		class4 = classNames.value(3)+sepCSV+eval.truePositiveRate(3)+sepCSV+eval.falsePositiveRate(3)+sepCSV+eval.precision(3)+sepCSV+eval.recall(3)+sepCSV+eval.fMeasure(3)+sepCSV+eval.areaUnderROC(3);
-		classifierName="J48 Default values all 160 attributes"+testMethod;
-		evalString += classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
-		
-		//*******************************J48 Default values AttributeSelection 33 attributes*******************************//
-				
-		
-//		AttributeSelection attsel = new AttributeSelection();  // package weka.filters.supervised.attribute!
-//		InfoGainAttributeEval infoGainEval = new InfoGainAttributeEval();
-//		Ranker ranker = new Ranker();
-//		ranker.setNumToSelect(-1);
-//		ranker.setThreshold(0);
-//	
-//		attsel.setEvaluator(infoGainEval);
-//		attsel.setSearch(ranker);
-//		try {
-//			attsel.SelectAttributes(train);
-//		} catch (Exception e2) {
-//			// TODO Auto-generated catch block
-//			e2.printStackTrace();
-//		}
-//		// obtain the attribute indices that were selected
-//		int[] indices = null;
-//		try {
-//			indices = attsel.selectedAttributes();
-//		} catch (Exception e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		System.out.println(Utils.arrayToString(indices));
-//		for(int i=0; i<indices.length; i++){
-//			System.out.println(train.attribute(indices[i]));
-//		}
-		
-//		AttributeSelectedClassifier classifier = new AttributeSelectedClassifier();
-//		InfoGainAttributeEval infoGainEval = new InfoGainAttributeEval();
-//		
-//		Ranker ranker = new Ranker();
-//		ranker.setNumToSelect(-1);
-//		ranker.setThreshold(0);
-//		
-//		j48 = new J48();
-//		try {
-//			j48.setOptions(j48Options);
-//		} catch (Exception e2) {
-//			// TODO Auto-generated catch block
-//			e2.printStackTrace();
-//		}
-//		classifier.setEvaluator(infoGainEval);
-//		classifier.setSearch(ranker);
-//		classifier.setClassifier(j48);
-//
-//		try {
-//			classifier.buildClassifier(train);
-//		} catch (Exception e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		try {
-//			eval = new Evaluation(train);
-//			eval.evaluateModel(classifier,train);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		classNames = train.classAttribute();
-//		class1 = classNames.value(0)+sepCSV+eval.truePositiveRate(0)+sepCSV+eval.falsePositiveRate(0)+sepCSV+eval.precision(0)+sepCSV+eval.recall(0)+sepCSV+eval.fMeasure(0);
-//		class2 = classNames.value(1)+sepCSV+eval.truePositiveRate(1)+sepCSV+eval.falsePositiveRate(1)+sepCSV+eval.precision(1)+sepCSV+eval.recall(1)+sepCSV+eval.fMeasure(1);
-//		class3 = classNames.value(2)+sepCSV+eval.truePositiveRate(2)+sepCSV+eval.falsePositiveRate(2)+sepCSV+eval.precision(2)+sepCSV+eval.recall(2)+sepCSV+eval.fMeasure(2);
-//		class4 = classNames.value(3)+sepCSV+eval.truePositiveRate(3)+sepCSV+eval.falsePositiveRate(3)+sepCSV+eval.precision(3)+sepCSV+eval.recall(3)+sepCSV+eval.fMeasure(3);
-//		classifierName="J48 Default values AttributeSelection 33 attributes";
-//		evalString += classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
-
+		evalString += j48Fct(eval, train, classNames, "TrainingIsTestData", !isFold, numFolds, randomSeed, isTrainingTest);
 			
-		//*******************************SMO Default values all 160 attributes*******************************//
+//*******************************SMO Default values all 160 attributes*******************************//
+		evalString += smoFct(eval, train, classNames, "TrainingIsTestData", !isFold, numFolds, randomSeed, isTrainingTest);
 
-		SMO smo = new SMO();
-//		String[] smoOptions = new String[18];
-//		smoOptions[0]="-C";//-C <double> The complexity constant C. (default 1)
-//		smoOptions[1]="1.0";
-//		smoOptions[2]="-L";//-L <double> The tolerance parameter. (default 1.0e-3)
-//		smoOptions[3]="0.001";
-//		smoOptions[4]="-P";//-P <double>The epsilon for round-off error. (default 1.0e-12)
-//		smoOptions[5]="1.0E-12";
-//		smoOptions[6]="-N";//-N Whether to 0=normalize/1=standardize/2=neither. (default 0=normalize)
-//		smoOptions[7]="0";
-//		smoOptions[8]="-V";//V <double> The number of folds for the internal cross-validation. (default -1, use training data)
-//		smoOptions[9]="-1"; 
-//		smoOptions[10]="-W";//-W <double> The random number seed. (default 1)
-//		smoOptions[11]="1";
-//		smoOptions[12]="-K";// -K <classname and parameters> The Kernel to use. (default: weka.classifiers.functions.supportVector.PolyKernel)
-//		smoOptions[13]="weka.classifiers.functions.supportVector.PolyKernel";
-//		smoOptions[14]="-C";//-C <num> The size of the cache (a prime number), 0 for full cache and -1 to turn it off. (default: 250007)
-//		smoOptions[15]="250007";
-//		smoOptions[16]="-E";//-E <num>  The Exponent to use.(default: 1.0)
-//		smoOptions[17]="1.0";
-//		smo.setOptions(smoOptions);
-		
-		try {
-			smo.buildClassifier(train);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			eval = new Evaluation(train);
-//			eval.crossValidateModel(smo,train,10,new Random(1));
-			eval.evaluateModel(smo,train);
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		classNames = train.classAttribute();
-		class1 = classNames.value(0)+sepCSV+eval.truePositiveRate(0)+sepCSV+eval.falsePositiveRate(0)+sepCSV+eval.precision(0)+sepCSV+eval.recall(0)+sepCSV+eval.fMeasure(0)+sepCSV+eval.areaUnderROC(0);
-		class2 = classNames.value(1)+sepCSV+eval.truePositiveRate(1)+sepCSV+eval.falsePositiveRate(1)+sepCSV+eval.precision(1)+sepCSV+eval.recall(1)+sepCSV+eval.fMeasure(1)+sepCSV+eval.areaUnderROC(1);
-		class3 = classNames.value(2)+sepCSV+eval.truePositiveRate(2)+sepCSV+eval.falsePositiveRate(2)+sepCSV+eval.precision(2)+sepCSV+eval.recall(2)+sepCSV+eval.fMeasure(2)+sepCSV+eval.areaUnderROC(2);
-		class4 = classNames.value(3)+sepCSV+eval.truePositiveRate(3)+sepCSV+eval.falsePositiveRate(3)+sepCSV+eval.precision(3)+sepCSV+eval.recall(3)+sepCSV+eval.fMeasure(3)+sepCSV+eval.areaUnderROC(3);
-		classifierName="SMO Default Values all 160 attributes"+testMethod;
-		evalString += classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
-		
 		
 		System.out.println(evalString);
 		
@@ -396,7 +199,153 @@ public class StartWeka {
 	}
 	
 	public static String naiveBayesFct(Evaluation ev, Instances tr, Attribute classNames, String testMethod, boolean crossvalidation, int folds, int randomSeed, boolean trainingIsTest){
-		return "";
+		NaiveBayes nB = new NaiveBayes();
+		try {
+			nB.buildClassifier(tr);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+		if(trainingIsTest){
+			try {
+				ev = new Evaluation(tr);
+				ev.evaluateModel(nB,tr);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(crossvalidation){
+			try {
+				ev.crossValidateModel(nB,tr,folds,new Random(1));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			
+//		Attribute classNames = train.classAttribute();
+		String class1 = classNames.value(0)+sepCSV+ev.truePositiveRate(0)+sepCSV+ev.falsePositiveRate(0)+sepCSV+ev.precision(0)+sepCSV+ev.recall(0)+sepCSV+ev.fMeasure(0)+sepCSV+ev.areaUnderROC(0);
+		String class2 = classNames.value(1)+sepCSV+ev.truePositiveRate(1)+sepCSV+ev.falsePositiveRate(1)+sepCSV+ev.precision(1)+sepCSV+ev.recall(1)+sepCSV+ev.fMeasure(1)+sepCSV+ev.areaUnderROC(1);
+		String class3 = classNames.value(2)+sepCSV+ev.truePositiveRate(2)+sepCSV+ev.falsePositiveRate(2)+sepCSV+ev.precision(2)+sepCSV+ev.recall(2)+sepCSV+ev.fMeasure(2)+sepCSV+ev.areaUnderROC(2);
+		String class4 = classNames.value(3)+sepCSV+ev.truePositiveRate(3)+sepCSV+ev.falsePositiveRate(3)+sepCSV+ev.precision(3)+sepCSV+ev.recall(3)+sepCSV+ev.fMeasure(3)+sepCSV+ev.areaUnderROC(3);
+		
+		String classifierName="NaiveBayes Default Values all 160 attributes"+testMethod;
+		String evString = ls+classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
+		return evString;
+	}
+	
+	public static String j48Fct(Evaluation ev, Instances tr, Attribute classNames, String testMethod, boolean crossvalidation, int folds, int randomSeed, boolean trainingIsTest){
+		J48 j48 = new J48();
+		String[] j48Options = new String[4];
+		j48Options[0]="-C";
+		j48Options[1]="0.25";
+		j48Options[2]="-M";
+		j48Options[3]="2";
+		try {
+			j48.setOptions(j48Options);
+		} catch (Exception e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		try {
+			j48.buildClassifier(tr);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		if(trainingIsTest){
+			try {
+				ev = new Evaluation(tr);
+				ev.evaluateModel(j48,tr);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(crossvalidation){
+			try {
+				ev.crossValidateModel(j48,tr,folds,new Random(1));			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		classNames = tr.classAttribute();
+		String class1 = classNames.value(0)+sepCSV+ev.truePositiveRate(0)+sepCSV+ev.falsePositiveRate(0)+sepCSV+ev.precision(0)+sepCSV+ev.recall(0)+sepCSV+ev.fMeasure(0)+sepCSV+ev.areaUnderROC(0);
+		String class2 = classNames.value(1)+sepCSV+ev.truePositiveRate(1)+sepCSV+ev.falsePositiveRate(1)+sepCSV+ev.precision(1)+sepCSV+ev.recall(1)+sepCSV+ev.fMeasure(1)+sepCSV+ev.areaUnderROC(1);
+		String class3 = classNames.value(2)+sepCSV+ev.truePositiveRate(2)+sepCSV+ev.falsePositiveRate(2)+sepCSV+ev.precision(2)+sepCSV+ev.recall(2)+sepCSV+ev.fMeasure(2)+sepCSV+ev.areaUnderROC(2);
+		String class4 = classNames.value(3)+sepCSV+ev.truePositiveRate(3)+sepCSV+ev.falsePositiveRate(3)+sepCSV+ev.precision(3)+sepCSV+ev.recall(3)+sepCSV+ev.fMeasure(3)+sepCSV+ev.areaUnderROC(3);
+		String classifierName="J48 Default values all 160 attributes"+testMethod;
+		String evString = classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
+		return evString;
+		
+
+	}
+		
+	public static String smoFct(Evaluation ev, Instances tr, Attribute classNames, String testMethod, boolean crossvalidation, int folds, int randomSeed, boolean trainingIsTest){
+		SMO smo = new SMO();
+//		String[] smoOptions = new String[18];
+//		smoOptions[0]="-C";//-C <double> The complexity constant C. (default 1)
+//		smoOptions[1]="1.0";
+//		smoOptions[2]="-L";//-L <double> The tolerance parameter. (default 1.0e-3)
+//		smoOptions[3]="0.001";
+//		smoOptions[4]="-P";//-P <double>The epsilon for round-off error. (default 1.0e-12)
+//		smoOptions[5]="1.0E-12";
+//		smoOptions[6]="-N";//-N Whether to 0=normalize/1=standardize/2=neither. (default 0=normalize)
+//		smoOptions[7]="0";
+//		smoOptions[8]="-V";//V <double> The number of folds for the internal cross-validation. (default -1, use training data)
+//		smoOptions[9]="-1"; 
+//		smoOptions[10]="-W";//-W <double> The random number seed. (default 1)
+//		smoOptions[11]="1";
+//		smoOptions[12]="-K";// -K <classname and parameters> The Kernel to use. (default: weka.classifiers.functions.supportVector.PolyKernel)
+//		smoOptions[13]="weka.classifiers.functions.supportVector.PolyKernel";
+//		smoOptions[14]="-C";//-C <num> The size of the cache (a prime number), 0 for full cache and -1 to turn it off. (default: 250007)
+//		smoOptions[15]="250007";
+//		smoOptions[16]="-E";//-E <num>  The Exponent to use.(default: 1.0)
+//		smoOptions[17]="1.0";
+//		smo.setOptions(smoOptions);
+		
+		try {
+			smo.buildClassifier(tr);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		if(trainingIsTest){
+			try {
+				ev = new Evaluation(tr);
+				ev.evaluateModel(smo,tr);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(crossvalidation){
+			try {
+				ev.crossValidateModel(smo,tr,folds,new Random(1));			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		classNames = tr.classAttribute();
+		String class1 = classNames.value(0)+sepCSV+ev.truePositiveRate(0)+sepCSV+ev.falsePositiveRate(0)+sepCSV+ev.precision(0)+sepCSV+ev.recall(0)+sepCSV+ev.fMeasure(0)+sepCSV+ev.areaUnderROC(0);
+		String class2 = classNames.value(1)+sepCSV+ev.truePositiveRate(1)+sepCSV+ev.falsePositiveRate(1)+sepCSV+ev.precision(1)+sepCSV+ev.recall(1)+sepCSV+ev.fMeasure(1)+sepCSV+ev.areaUnderROC(1);
+		String class3 = classNames.value(2)+sepCSV+ev.truePositiveRate(2)+sepCSV+ev.falsePositiveRate(2)+sepCSV+ev.precision(2)+sepCSV+ev.recall(2)+sepCSV+ev.fMeasure(2)+sepCSV+ev.areaUnderROC(2);
+		String class4 = classNames.value(3)+sepCSV+ev.truePositiveRate(3)+sepCSV+ev.falsePositiveRate(3)+sepCSV+ev.precision(3)+sepCSV+ev.recall(3)+sepCSV+ev.fMeasure(3)+sepCSV+ev.areaUnderROC(3);
+		String classifierName="SMO Default Values all 160 attributes"+testMethod;
+		String evString = classifierName+ls+class1+ls+class2+ls+class3+ls+class4+ls+ls;
+		return evString;
 	}
 
 
